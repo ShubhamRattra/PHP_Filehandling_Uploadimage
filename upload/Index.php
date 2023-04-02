d5632dda2d49b4f6850e938988918e01eb694379
Shubham Rattra <46563834+ShubhamRattra@users.noreply.github.com>
2023-04-02 22:42:36 +0530

    Add files via upload

---
+++ upload/Index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+	<h1>File Upload</h1>
+
+	<form action="upload.php" method="post" enctype="multipart/form-data">
+		Select image to upload:
+		  <input type="file" name="fileToUpload" id="fileToUpload"> <br> <br>
+		  <input type="submit" value="Upload Image" name="submit">
+	</form>
+
+</body>
+
+</html>
